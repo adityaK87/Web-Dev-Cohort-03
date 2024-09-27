f8063550d6b20e86b4c178b9983c87b0a9f5b568
Aditya Kumar <adityapal1409@gmail.com>
2024-09-27 11:43:54 +0530

    week-8-admin-router

---
+++ week-8-practice-of-backend/routes/admin.js
@@ -0,0 +1,35 @@
+const { Router } = require("express");
+const adminRouter = Router();
+adminRouter.post("/signup", (req, res) => {
+	res.json({
+		message: "User signed up successfully",
+	});
+});
+
+adminRouter.post("/signin", (req, res) => {
+	res.json({
+		message: "User signed in successfully",
+	});
+});
+
+adminRouter.post("/course", (req, res) => {
+	res.json({
+		message: "create course endpoint",
+	});
+});
+
+adminRouter.put("/couse/:courseId", (req, res) => {
+	res.json({
+		message: "change course",
+	});
+});
+
+adminRouter.get("/bulk", (req, res) => {
+	res.json({
+		message: "all the bulk courses",
+	});
+});
+
+module.exports = {
+	adminRouter: adminRouter,
+};
