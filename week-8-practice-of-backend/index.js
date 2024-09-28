c9531444913ee322d17e01ec134cc08a0f51eb14
Aditya Kumar <adityapal1409@gmail.com>
2024-09-28 20:39:55 +0530

    week 8 business logic

--- week-8-practice-of-backend/index.js
+++ week-8-practice-of-backend/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 
 const app = express();
+app.use(express.json());
 
-app.use("/user", userRouter);
-app.use("/admin", adminRouter);
-app.use("/courses", courseRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/courses", courseRouter);
 
-app.listen(3000, () => {
-	console.log("listening on http://localhost:3000");
-});
+const main = async () => {
+	await mongoose.connect(
+		"mongodb+srv://adityapal1409:8UQ4wT192KCrUIuj@cluster0.l39r0.mongodb.net/couse-selling"
+	);
+	console.log("Database connection established");
+	app.listen(3000, () => {
+		console.log("listening on http://localhost:3000");
+	});
+};
+main();
--- week-8-practice-of-backend/routes/user.js
+++ week-8-practice-of-backend/routes/user.js
@@ -1,17 +1,83 @@
+require("dotenv").config();
 const { Router } = require("express");
+const { UserModel } = require("../db");
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userRouter = Router();
 
-userRouter.post("/signup", (req, res) => {
-	res.json({
-		message: "User signed up successfully",
-	});
+const requiredBody = z.object({
+	email: z.string().email().min(10).max(100),
+	password: z.string().min(10).max(100),
+	firstName: z.string().min(3).max(100),
+	lastName: z.string().min(3).max(100),
 });
 
-userRouter.post("/signin", (req, res) => {
-	res.json({
-		message: "User signed in successfully",
-	});
+userRouter.post("/signup", async (req, res) => {
+	try {
+		const parsedData = requiredBody.safeParse(req.body);
+		if (!parsedData.success) {
+			res.json({
+				messsage: "Signup failed",
+				errors: parsedData.error,
+			});
+			return;
+		}
+		const { email, password, firstName, lastName } = parsedData.data;
+		const hashedPassword = await bcrypt.hash(password, 5);
+
+		await UserModel.create({
+			email,
+			password: hashedPassword,
+			firstName,
+			lastName,
+		});
+		res.json({
+			message: "Signed up successfully",
+		});
+	} catch (error) {
+		res.json({
+			message: "User already exists",
+		});
+	}
+});
+
+userRouter.post("/signin", async (req, res) => {
+	try {
+		const parsedData = requiredBody.safeParse(req.body);
+		const { email, password } = parsedData.data;
+		const foundUser = await UserModel.findOne({ email });
+		if (!foundUser) {
+			res.json({
+				message: "User not found",
+			});
+			return;
+		}
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			foundUser.password
+		);
+		if (!isPasswordCorrect) {
+			res.json({
+				message: "Password is incorrect",
+			});
+			return;
+		}
+		// console.log(foundUser._id.toString());
+		console.log("jwt scret", process.env.JWT_SECRET_PASS);
+		const token = jwt.sign(
+			{ userId: foundUser._id.toString() },
+			process.env.JWT_SECRET_PASS
+		);
+		res.json({
+			token: token,
+		});
+	} catch (error) {
+		res.json({
+			message: "",
+		});
+	}
 });
 
 userRouter.post("/purchases", (req, res) => {
