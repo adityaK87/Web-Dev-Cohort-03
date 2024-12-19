076ed0093713ef4ea36362fae5cec3cc102ed78d
Aditya Kumar <adityapal1409@gmail.com>
2024-12-19 13:39:21 +0530

    Input

--- brainly-frontend/src/components/CreateContentModel.tsx
+++ brainly-frontend/src/components/CreateContentModel.tsx
@@ -1,5 +1,6 @@
 import CrossIcon from "../icons/CrossIcon";
 import { Button } from "./Button";
+import Input from "./Input";
 
 export interface CreateContentProps {
 	isOpen: boolean;
@@ -39,22 +40,4 @@ const CreateContentModel = ({ isOpen, setClose }: CreateContentProps) => {
 	);
 };
 
-const Input = ({
-	onChange,
-	placeholder,
-}: {
-	onChange: () => void;
-	placeholder: string;
-}) => {
-	return (
-		<div>
-			<input
-				type="text"
-				placeholder={placeholder}
-				onChange={onChange}
-				className="px-4 py-2 border rounded m-2 text-black"
-			/>
-		</div>
-	);
-};
 export default CreateContentModel;
