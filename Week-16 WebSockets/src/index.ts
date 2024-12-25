de2bdfac1f8de278c0cab69507679fd33137cedf
Aditya Kumar <adityapal1409@gmail.com>
2024-12-25 23:29:02 +0530

    Week-16 Ping pong

--- Week-16 WebSockets/src/index.ts
+++ Week-16 WebSockets/src/index.ts
@@ -3,11 +3,9 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
-	console.log("Connection Established");
-	socket.send("data send");
-	setInterval(() => {
-		socket.send("Price of solana is " + Math.random());
-	}, 500);
-
-	socket.on("message", (e) => console.log(e.toString()));
+	socket.on("message", (e) => {
+		if (e.toString() === "ping") {
+			socket.send("pong");
+		}
+	});
 });
---
+++ Week-16 WebSockets/ws fe/src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+	const [socket, setSocket] = useState();
+	const inputRef = useRef<HTMLInputElement>();
+
+	function sendMessage() {
+		if (!socket) {
+			return;
+		}
+
+		const message = inputRef.current?.value;
+		socket.send(message);
+	}
+
+	useEffect(() => {
+		const ws = new WebSocket("ws://localhost:8080");
+		setSocket(ws);
+		ws.onmessage = (ev) => {
+			alert(ev.data);
+		};
+	}, []);
+	return (
+		<div>
+			<input type="text" placeholder="Message" ref={inputRef} />
+			<button onClick={sendMessage}>Send</button>
+		</div>
+	);
+}
+
+export default App;
