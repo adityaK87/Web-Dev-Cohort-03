8bd7496d02cf895c8e19b66e4bda558b9022f1e3
Aditya Kumar <adityapal1409@gmail.com>
2025-02-04 20:37:11 +0530

    week 18 Relationships in Prisma

--- Week-18 Prisma and ORMs/Class-18.1/src/app.ts
+++ Week-18 Prisma and ORMs/Class-18.1/src/app.ts
@@ -1,15 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
 
 const client = new PrismaClient();
 
-async function main() {
-	await client.user.create({
-		data: {
-			email: "adityapal@gmail.com",
-			username: "Adityak87",
-			password: "123@123",
+app.get("/users", async (req, res) => {
+	const users = await client.user.findMany();
+	res.send(users);
+});
+
+app.get("/todos/:id", async (req, res) => {
+	const id = req.params.id as unknown as number;
+	const todos = await client.todo.findMany({
+		where: {
+			userId: Number(id),
 		},
 	});
-}
+	res.send(todos);
+});
 
-main();
+app.listen(3000);
