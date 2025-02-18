9a9f701d2085b53febb16399b39189b3f222d700
Aditya Kumar <adityapal1409@gmail.com>
2025-02-18 15:10:32 +0530

    create sign in end point

--- Week-22 Draw App/my-turborepo/apps/http-server/src/index.ts
+++ Week-22 Draw App/my-turborepo/apps/http-server/src/index.ts
@@ -13,13 +13,29 @@ import { prismaClient } from "@repo/db/prisma";
 const app = express();
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
-	const data = CreateUserSchema.safeParse(req.body);
-	if (!data.success) {
+app.post("/signup", async (req, res) => {
+	const parsedData = CreateUserSchema.safeParse(req.body);
+	if (!parsedData.success) {
 		res.json({
 			message: "Invalid data",
 		});
 		return;
+	}
+	try {
+		await prismaClient.user.create({
+			data: {
+				email: parsedData.data.username,
+				password: parsedData.data.password,
+				name: parsedData.data.name,
+			},
+		});
+		res.status(200).json({
+			message: "User Created Successfully",
+		});
+	} catch (err) {
+		res.status(411).json({
+			message: "User Already Exists",
+		});
 	}
 });
 
